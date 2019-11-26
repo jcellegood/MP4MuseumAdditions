2c637e7e65eb6b1137920ce4b5fc435e8d8daa67
jcellegood <jcellegood@gmail.com>
2019-11-25 22:02:54 -0500

    initial upload
    this version shuffles the file list before starting the play loop

---
+++ mp4museum.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+# mp4museum.org by julius schmiedel 2019
+
+import os
+import sys
+import glob
+from subprocess import Popen, PIPE
+import RPi.GPIO as GPIO
+from random import shuffle
+
+FNULL = open(os.devnull, "w")
+
+# setup GPIO pin
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(13, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+# functions to be called by event listener
+def buttonPause(channel):
+	player.stdin.write("p")
+
+def buttonNext(channel):
+	player.stdin.write("q")
+
+# add event listener
+GPIO.add_event_detect(11, GPIO.FALLING, callback = buttonPause, bouncetime = 234)
+GPIO.add_event_detect(13, GPIO.FALLING, callback = buttonNext, bouncetime = 1234)
+
+# please do not remove my logo screen
+player = Popen(['omxplayer', '--adev', 'both', '/home/pi/mp4museum.mp4'],stdin=PIPE,stdout=FNULL)
+player.wait()
+
+# get the list of files and shuffle them
+filelist = sorted(glob.glob(r'/media/*/*.mp4'))
+shuffle(filelist)
+
+# the loop
+while(1):
+    for files in filelist:
+        player = Popen(['omxplayer','--adev', 'both',files],stdin=PIPE,stdout=FNULL)
+        player.wait()
